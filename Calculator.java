4bf5a0f3362229d7dac44e3fec9d5b56aa84248b
serhii <y7913092_w@iespoblenou.org>
2024-12-03 09:17:19 +0100

    Add modulus method

--- Calculator.java
+++ Calculator.java
@@ -13,6 +13,8 @@ public class Calculator {
         System.out.println("Multiplicacion: " + calculator.multiply(10, 5));
         System.out.println("Division: " + calculator.divide(10, 5));
         System.out.println("Division por cero: " + calculator.divide(10, 0));
+        System.out.println("Modulus: " + calculator.modulus(10, 3));
+        System.out.println("Modulus by zero: " + calculator.modulus(10, 0));
 
     }
     public int multiply(int a, int b) {
@@ -24,6 +26,13 @@ public class Calculator {
             return 0;
         }
         return a / b;
+    }
+    public int modulus(int a, int b) {
+        if (b == 0) {
+            System.out.println("Error: Cannot perform modulus with divisor zero.");
+            return 0;
+        }
+        return a % b;
     }
 
 
